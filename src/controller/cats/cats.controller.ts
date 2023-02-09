01cb5250b0380c0ec37307c1ac68698fb5de1cc0
zhangshun <zhangshun@growingio.com>
2023-02-09 20:55:28 +0800

    feat: 请求demo

--- src/controller/cats/cats.controller.ts
+++ src/controller/cats/cats.controller.ts
@@ -1,15 +1,24 @@
-import { Controller, Get, Post, Req } from '@nestjs/common';
+import { Controller, Get, Header, HttpCode, Post, Req } from '@nestjs/common';
 import { Request } from 'express';
 
 @Controller('cats')
 export class CatsController {
+  //** post /cats */
   @Post()
+  @HttpCode(202)
+  @Header('Cache-Control', 'none')
   create(): string {
     return 'This action add a new cat';
   }
 
+  //** get /cats */
   @Get()
   findAll(@Req() request: Request): string {
     return 'This action returns all cats!';
+  }
+  //** get /cats/ab* */
+  @Get('ab*')
+  find(): string {
+    return 'This action only return ab* cats';
   }
 }
