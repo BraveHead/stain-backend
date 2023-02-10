28bf523ada7d082cc45a8e0672c33cf42991bead
zhangshun <zhangshun@growingio.com>
2023-02-10 10:53:07 +0800

    feat: add hostController demo

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AppController } from './controller/app/app.controller';
 import { CatsController } from '@/controller/cats/cats.controller';
+import { AccountController } from './controller/host/host.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, CatsController],
+  controllers: [AppController, CatsController, AccountController],
   providers: [AppService],
 })
 export class AppModule {}
