30456904676438218253362bdf52dc800384af62
zhangshun <zhangshun@growingio.com>
2023-02-16 17:23:38 +0800

    feat: add middleware demo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,9 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 // import { AppController } from './controller/app.controller';
 import { AppService } from './app.service';
 import { AppController } from './controller/app/app.controller';
@@ -6,10 +11,25 @@ import { CatsController } from '@/cats/cats.controller';
 import { AccountController } from './controller/host/host.controller';
 import { CatsService } from './cats/cats.service';
 import { CatsModule } from './cats/cats.module';
+import {
+  logger,
+  LoggerMiddleware,
+} from './common/middleware/logger.middleware';
 
 @Module({
   imports: [CatsModule],
   controllers: [AppController, CatsController, AccountController],
   providers: [AppService, CatsService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  async configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(logger, LoggerMiddleware)
+      .exclude(
+        { path: 'cats', method: RequestMethod.GET },
+        { path: 'cats', method: RequestMethod.POST },
+        'cats/(.*)',
+      )
+      .forRoutes(CatsController);
+  }
+}
